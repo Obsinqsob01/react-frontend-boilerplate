13f24d8d93d90755e9e01d39b407708f5f5bec4d
Luis Contreras Guerra <luis@mailvibes.io>
2020-11-21 15:03:10 -0600

    Improve error handling

--- src/Components/UserForm/index.js
+++ src/Components/UserForm/index.js
@@ -13,39 +13,45 @@ export const UserFormLogin = ({ disabled, error, title, onSubmit }) => {
   }
 
   return (
-    <Centered width="50%">
-      {error && <Error>{error}</Error>}
-      <Form disabled={disabled} onSubmit={handleSubmit}>
-        <Title>{title}</Title>
-        <Input placeholder='Email' {...email} />
-        <Input placeholder='Password' type='password' {...password} />
-        <Button onClick={handleSubmit}>{title}</Button>
-      </Form>
-    </Centered>
+    <>
+      {error.length > 1 && typeof error === Array? error.map(err => <Error key={err}>- {err}</Error>): error && <Error>{error}</Error>}
+      <Centered width="50%">
+        <Form disabled={disabled} onSubmit={handleSubmit}>
+          <Title>{title}</Title>
+          <Input placeholder='Email' {...email} />
+          <Input placeholder='Password' type='password' {...password} />
+          <Button onClick={handleSubmit}>{title}</Button>
+        </Form>
+      </Centered>
+    </>
   )
 }
 
 export const UserFormRegister = ({ disabled, error, title, onSubmit }) => {
   const name = useInputValue('')
+  const lastName = useInputValue('')
   const email = useInputValue('')
   const password = useInputValue('')
 
   const handleSubmit = e => {
     e.preventDefault()
-    onSubmit({ name: name.value, email: email.value, password: password.value })
+    onSubmit({ name: name.value, lastName: lastName.value, email: email.value, password: password.value })
   }
 
   return (
-    <Centered width="50%">
-      {error && <Error>{error}</Error>}
-      <Form disabled={disabled} onSubmit={handleSubmit}>
-        <Title>{title}</Title>
-        <Input placeholder='Name' {...name} />
-        <Input placeholder='Email' {...email} type="email" />
-        <Input placeholder='Password' type='password' {...password} />
-        <Button onClick={handleSubmit}>{title}</Button>
-      </Form>
-    </Centered>
+    <>
+      {error.length > 1 && typeof error === Array? error.map(err => <Error key={err}>- {err}</Error>): error && <Error>{error}</Error>}
+      <Centered width="50%">
+        <Form disabled={disabled} onSubmit={handleSubmit}>
+          <Title>{title}</Title>
+          <Input placeholder='Name' {...name} />
+          <Input placeholder='Last Name' {...lastName} />
+          <Input placeholder='Email' {...email} type="email" />
+          <Input placeholder='Password' type='password' {...password} />
+          <Button onClick={handleSubmit}>{title}</Button>
+        </Form>
+      </Centered>
+    </>
   )
 }
 
